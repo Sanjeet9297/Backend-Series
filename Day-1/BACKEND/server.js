5d7089a5bab9ee75362552565fe72c2678d0065a
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-05-27 23:37:47 +0530

    new commit

--- Day-1/BACKEND/server.js
+++ Day-1/BACKEND/server.js
@@ -2,14 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.use(express.static("dist"));
+
 // app.get("/", (req, res) => {
 //   res.send("server is ready");
 // });
 
 // get a list of 5 jokes
 
-
-app.get("/jokes", (req, res) => {
+app.get("/api/jokes", (req, res) => {
   const jokes = [
     {
       id: 1,
